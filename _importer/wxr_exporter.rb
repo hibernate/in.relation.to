0a87bbba809b4c945fcd72fb45231c2668bb5ea9
Hardy Ferentschik <hardy@hibernate.org>
2015-03-28 01:07:28 +0100

    WEBSITE-266 Making sure that comments are attached to right post. Post and comments were out of sync.

--- _importer/wxr_exporter.rb
+++ _importer/wxr_exporter.rb
@@ -7,12 +7,6 @@ class WxrExporter
 	# reference to the WXR template file
 	@doc
 
-	# reference to the original item node (used as template)
-	@master_item
-
-    # reference to the oringal comment node (used as template)
-	@master_comment
-
 	# the current item (blog post) to create comments for
 	@current_item
 
@@ -26,68 +20,77 @@ class WxrExporter
 		file = File.open(wxr_template_path)
 		@doc = Nokogiri::XML(file)
 		file.close
-
-        # get the item node
-		@master_item = @doc.at_xpath("//item")
-		@master_item.xpath('//comment()').remove
-
-        # get the comment node
-		@master_comment = @master_item.at_xpath("wp:comment")
-		@master_comment.at_xpath("//dsq:remote").remove
-		@master_comment.unlink
 	end
 
 	def create_item(title, url, body, id, creation_time)
-		item = @master_item.dup
-		item.at_xpath("//title").content = title
-		item.at_xpath("//link").content = url
-		item.at_xpath("//content:encoded").children.find{|node|
-			if node.cdata?
-				node.content = body
-			end
-		}
-		item.at_xpath("//dsq:thread_identifier").content = creation_time.strftime('%d-%m-%Y') + '-' + id
-		item.at_xpath("//wp:post_date_gmt").content = creation_time.strftime("%Y-%m-%d %H:%M:%S")
-		@master_item.after item
-		@current_item = item
+        item = Nokogiri::XML::Node.new "item", @doc
+
+        title_node = Nokogiri::XML::Node.new "title", @doc
+        title_node.content = title
+        item.add_child title_node
+
+        link = Nokogiri::XML::Node.new "link", @doc
+        link.content = url
+        item.add_child link
+
+        id = Nokogiri::XML::Node.new "dsq:thread_identifier", @doc
+        id.content = creation_time.strftime('%d-%m-%Y') + '-' + id
+        item.add_child id
+
+        date = Nokogiri::XML::Node.new "wp:post_date_gmt", @doc
+        date.content = creation_time.strftime("%Y-%m-%d %H:%M:%S")
+        item.add_child date
+
+        content = Nokogiri::XML::Node.new "content:encoded", @doc
+        content.add_child @doc.create_cdata(body)
+        item.add_child content
+
+		@current_item = @doc.at_xpath("//channel").add_child item
 	end
 
 	def add_comment(id, author, email, url, creation_time, content)
-		if @current_item.nil?
-			abort "ERROR: There is no item for adding a comment"
-		end
+		comment = Nokogiri::XML::Node.new "wp:comment", @doc
 
-		comment = @master_comment.dup
+        comment_id = Nokogiri::XML::Node.new "wp:comment_id", @doc
+        comment_id.content = id
+        comment.add_child comment_id
 
-		comment.at_xpath("wp:comment_id").content = id
-		comment.at_xpath("wp:comment_author").content = author
-		comment.at_xpath("wp:comment_author_email").content = email
-		comment.at_xpath("wp:comment_author_url").content = url
-		comment.at_xpath("wp:comment_date_gmt").content = creation_time.strftime("%Y-%m-%d %H:%M:%S")
-		comment.at_xpath("wp:comment_content").children.find{|node|
-			if node.cdata?
-				node.content = content
-			end
-		}
+        comment_author = Nokogiri::XML::Node.new "wp:comment_author", @doc
+        comment_author.content = author
+        comment.add_child comment_author
 
-        #append the comment to the current item
-		@current_item << comment
+        comment_author_email = Nokogiri::XML::Node.new "wp:comment_author_email", @doc
+        comment_author_email.content = email
+        comment.add_child comment_author_email
+
+        comment_author_url = Nokogiri::XML::Node.new "wp:comment_author_url", @doc
+        comment_author_url.content = url
+        comment.add_child comment_author_url
+
+        comment_date = Nokogiri::XML::Node.new "wp:comment_date_gmt", @doc
+        comment_date.content = creation_time.strftime("%Y-%m-%d %H:%M:%S")
+        comment.add_child comment_date
+
+        comment_content = Nokogiri::XML::Node.new "wp:comment_content", @doc
+        comment_content.add_child @doc.create_cdata(content)
+        comment.add_child comment_content
+
+		@current_item.add_child comment
 	end
 
 	def write_wxr
 		# unlink the original master node. It was just used as a template
-		@master_item.unlink
 		File.open(@output_path, 'w') { |f| f.print(@doc.to_xml) }
 	end
 end
 
-# wxr_exporter = WxrExporter.new "./disqus.xml", "comments-wxr.xml"
-# wxr_exporter.create_item("Hello world", "http://hello/world", "blah < >", "1", DateTime.now)
-# wxr_exporter.add_comment("1", "Author 1", "foo@mailinator.com", "http://foobar.com", DateTime.now, "Test comment")
-# wxr_exporter.add_comment("2", "Author 2", "foo@mailinator.com", "http://foobar.com", DateTime.now, "Test comment")
+wxr_exporter = WxrExporter.new "./disqus.xml", "comments-wxr.xml"
+wxr_exporter.create_item("Hello world", "http://hello/world", "blah < >", "1", DateTime.now)
+wxr_exporter.add_comment("1", "Author 1", "foo@mailinator.com", "http://foobar.com", DateTime.now, "Test comment")
+wxr_exporter.add_comment("2", "Author 2", "foo@mailinator.com", "http://foobar.com", DateTime.now, "Test comment")
 
-# wxr_exporter.create_item("Snafu
-# 	", "http://hello/world", "blah < >", "1", DateTime.now)
-# wxr_exporter.add_comment("3", "Author 3", "foo@mailinator.com", "http://foobar.com", DateTime.now, "Test comment")
+wxr_exporter.create_item("Snafu
+	", "http://hello/world", "blah < >", "1", DateTime.now)
+wxr_exporter.add_comment("3", "Author 3", "foo@mailinator.com", "http://foobar.com", DateTime.now, "Test comment")
 
-# wxr_exporter.write_wxr
+wxr_exporter.write_wxr
