9930033bc0feb7402ca7d76ab804067966749708
Yoann Rodière <yoann@hibernate.org>
2023-01-11 18:18:07 +0100

    Move to Google Analytics 4

--- _ext/pipeline.rb
+++ _ext/pipeline.rb
@@ -1,6 +1,7 @@
 require 'js_minifier'
 require 'css_minifier'
 require 'html_minifier'
+require 'google_analytics_4'
 require 'relative'
 require 'redirect_creator'
 require 'directory_listing'
@@ -53,7 +54,7 @@ end
 Awestruct::Extensions::Pipeline.new do
   # register helpers to be used in templates
   helper Awestruct::Extensions::Partial
-  helper Awestruct::Extensions::GoogleAnalytics
+  helper Awestruct::Extensions::GoogleAnalytics4
   helper Awestruct::Extensions::Relative
   helper Awestruct::Extensions::DirectoryListing
 
